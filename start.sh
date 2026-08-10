9f113c1b43d5f19654452b436a6e218974ff811f
mino <unclevol@outlook.com>
2026-08-10 15:25:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ export PATH="$HOME/.local/bin:$PATH"
 export PYTHONPATH="$HOME/.local/lib/python3.11/site-packages:$HOME/.local/lib/python3.10/site-packages:$PYTHONPATH"
 
 # 平台如果注入了 PORT 就用平台的，否则 5000
-export PORT="${PORT:-5000}"
+export PORT="${PORT:-8080}"
 
 echo "[sub-aggregator] HOME=$HOME PORT=$PORT" > /tmp/bot.log
 echo "[sub-aggregator] PYTHONPATH=$PYTHONPATH" >> /tmp/bot.log
